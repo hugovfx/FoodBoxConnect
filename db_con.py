f68d573281919ffc45f1ff8ef93eca42c6a01d79
HugoVFX <hugogamesbaezq@gmail.com>
2025-03-13 17:21:46 -0600

    configuracion de uso de .env en url mongo

--- db_con.py
+++ db_con.py
@@ -1,6 +1,10 @@
 import pymongo
+import os
 
-url = API_KEY_MONGODB
+url = os.getenv('API_KEY_MONGODB')
+
+if not url:
+    raise ValueError("La URL de MongoDB no está configurada correctamente en las variables de entorno")
 
 client = pymongo.MongoClient(url)
 db = client['foodboxconnect']
