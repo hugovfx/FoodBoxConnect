556234772b4b327316d185e93f69effcf106a77f
HugoVFX <hugogamesbaezq@gmail.com>
2025-03-31 22:57:04 -0600

    tmp

--- db_con.py
+++ db_con.py
@@ -1,13 +1,18 @@
 import pymongo
 import os
 from dotenv import load_dotenv
+
+# Cargar variables de entorno
 load_dotenv()
 
-
-url = os.getenv('API_KEY_MONGODB')
+# Obtener la URL de MongoDB desde las variables de entorno
+url = os.getenv('MONGODB_URI')
 
 if not url:
     raise ValueError("La URL de MongoDB no está configurada correctamente en las variables de entorno")
 
+# Conectar a la base de datos de MongoDB
 client = pymongo.MongoClient(url)
+
+# Especificar la base de datos que deseas usar
 db = client['foodboxconnect']
